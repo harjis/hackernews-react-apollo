c541cb616d8eea6df342aa86416f4d028354df58
Joonas Harjukallio <harjukallio@gmail.com>
2017-09-26 21:15:31 +0300

    Maybe this

--- src/index.js
+++ src/index.js
@@ -2,41 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
-import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+import { ApolloProvider, ApolloClient } from 'react-apollo';
+import { ApolloLink, HttpLink, WebSocketLink } from 'apollo-link';
+import { getOperationAST } from 'graphql';
 
 import App from './components/App';
 import './styles/index.css';
 import { GC_AUTH_TOKEN } from './constans';
 
-const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/cj7rts37j06np0146ramr9opm'
-});
-
-const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj7rts37j06np0146ramr9opm', {
-  reconnect: true,
-  connectionParams: {
-    authToken: localStorage.getItem(GC_AUTH_TOKEN)
-  }
-});
-
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(networkInterface, wsClient);
-
-networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      if (!req.options.headers) {
-        req.options.headers = {};
-      }
-      const token = localStorage.getItem(GC_AUTH_TOKEN);
-      req.options.headers.authorization = token ? `Bearer ${token}` : null;
-      next();
+const uri = 'https://api.graph.cool/simple/v1/cj7rts37j06np0146ramr9opm';
+const wsClient = {
+  uri: 'wss://subscriptions.graph.cool/v1/cj7rts37j06np0146ramr9opm',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN)
     }
   }
-]);
+};
+
+const link = ApolloLink.split(
+  operation => {
+    // check if it is a subscription
+    const operationAST = getOperationAST(operation.query, operation.operationName);
+    return !!operationAST && operationAST.operation === 'subscription';
+  },
+  new WebSocketLink(wsClient),
+  new HttpLink({ uri })
+);
 
 const client = new ApolloClient({
-  networkInterface: networkInterfaceWithSubscriptions
+  networkInterface: link
 });
 
 ReactDOM.render(
