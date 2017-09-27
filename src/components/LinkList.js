8a50f14a6d1aaefd0f523e0905db718b6265aa24
Joonas Harjukallio <harjukallio@gmail.com>
2017-09-27 20:19:38 +0300

    Subscribe to votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,6 +6,7 @@ import Link from './Link';
 class LinkList extends Component {
   componentDidMount() {
     this._subscribeToNewLinks();
+    this._subscribeToNewVotes();
   }
 
   _subscribeToNewLinks = () => {
@@ -36,6 +37,54 @@ class LinkList extends Component {
       `,
       updateQuery: (previous, { subscriptionData }) => {
         const newAllLinks = [subscriptionData.data.Link.node, ...previous.allLinks];
+        const result = {
+          ...previous,
+          allLinks: newAllLinks
+        };
+        return result;
+      }
+    });
+  };
+
+  _subscribeToNewVotes = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+          subscription {
+              Vote(filter: {
+                  mutation_in: [CREATED]
+              }) {
+                  node {
+                      id
+                      link {
+                          id
+                          url
+                          description
+                          createdAt
+                          postedBy {
+                              id
+                              name
+                          }
+                          votes {
+                              id
+                              user {
+                                  id
+                              }
+                          }
+                      }
+                      user {
+                          id
+                      }
+                  }
+              }
+          }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const votedLinkIndex = previous.allLinks.findIndex(
+          link => link.id === subscriptionData.data.Vote.node.link.id
+        );
+        const link = subscriptionData.data.Vote.node.link;
+        const newAllLinks = previous.allLinks.slice();
+        newAllLinks[votedLinkIndex] = link;
         const result = {
           ...previous,
           allLinks: newAllLinks
