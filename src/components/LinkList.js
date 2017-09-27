cd90006c24804da6c1d957190452bd813f29be02
Joonas Harjukallio <harjukallio@gmail.com>
2017-09-27 20:39:13 +0300

    Pagination navigation

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql, gql } from 'react-apollo';
 
 import Link from './Link';
-import { LINKS_PER_PAGE } from "../constans";
+import { GC_AUTH_TOKEN, GC_USER_ID, LINKS_PER_PAGE } from '../constans';
 
 class LinkList extends Component {
   componentDidMount() {
@@ -95,8 +95,39 @@ class LinkList extends Component {
     });
   };
 
+  _getLinksToRender = isNewPage => {
+    if (isNewPage) {
+      return this.props.allLinksQuery.allLinks;
+    }
+    const rankedLinks = this.props.allLinksQuery.allLinks.slice();
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length);
+    return rankedLinks;
+  };
+
+  _nextPage = () => {
+    const page = parseInt(this.props.match.params.page, 10);
+    if (page <= this.props.allLinksQuery._allLinksMeta.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1;
+      this.props.history.push(`/new/${nextPage}`);
+    }
+  };
+
+  _previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10);
+    if (page > 1) {
+      const previousPage = page - 1;
+      this.props.history.push(`/new/${previousPage}`);
+    }
+  };
+
   _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: ALL_LINKS_QUERY });
+    const isNewPage = this.props.location.pathname.includes('new');
+    const page = parseInt(this.props.match.params.page, 10);
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+    const first = isNewPage ? LINKS_PER_PAGE : 100;
+    const orderBy = isNewPage ? 'createdAt_DESC' : null;
+    const data = store.readQuery({ query: ALL_LINKS_QUERY, variables: { first, skip, orderBy } });
+
     const votedLink = data.allLinks.find(link => link.id === linkId);
     votedLink.votes = createVote.link.votes;
     store.writeQuery({ query: ALL_LINKS_QUERY, data });
@@ -108,26 +139,56 @@ class LinkList extends Component {
     }
 
     if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
-      return (
-        <div>
-          Error:
-          {this.props.allLinksQuery.error.message}
-        </div>
-      );
+      return <div>Error</div>;
     }
 
-    const linksToRender = this.props.allLinksQuery.allLinks;
+    const isNewPage = this.props.location.pathname.includes('new');
+    const linksToRender = this._getLinksToRender(isNewPage);
+    const userId = localStorage.getItem(GC_USER_ID);
 
     return (
       <div>
-        {linksToRender.map((link, index) =>
-          <Link
-            key={link.id}
-            index={index}
-            link={link}
-            updateStoreAfterVote={this._updateCacheAfterVote}
-          />
-        )}
+        {!userId
+          ? <button
+              onClick={() => {
+                this.props.history.push('/login');
+              }}
+            >
+              Login
+            </button>
+          : <div>
+              <button
+                onClick={() => {
+                  this.props.history.push('/create');
+                }}
+              >
+                New Post
+              </button>
+              <button
+                onClick={() => {
+                  localStorage.removeItem(GC_USER_ID);
+                  localStorage.removeItem(GC_AUTH_TOKEN);
+                  this.forceUpdate(); // doesn't work as it should :(
+                }}
+              >
+                Logout
+              </button>
+            </div>}
+        <div>
+          {linksToRender.map((link, index) =>
+            <Link
+              key={link.id}
+              updateStoreAfterVote={this._updateCacheAfterVote}
+              link={link}
+              index={index}
+            />
+          )}
+        </div>
+        {isNewPage &&
+          <div>
+            <button onClick={() => this._previousPage()}>Previous</button>
+            <button onClick={() => this._nextPage()}>Next</button>
+          </div>}
       </div>
     );
   }
